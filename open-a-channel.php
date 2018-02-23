e3ea5c2c1f73797fa78f1d14920f6f8827253b42
Dylan McDonald <dmcdonald21@cnm.edu>
2018-02-23 11:42:22 -0700

    created a starter API

---
+++ open-a-channel.php
@@ -0,0 +1,30 @@
+<?php
+require_once("vendor/autoload.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use PubNub\{PNConfiguration, PubNub};
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	$config = readConfig("/etc/apache2/capstone-mysql/kmaru.ini");
+	$pubNub = json_decode($config["pubnub"]);
+	$pubNubConf = new PNConfiguration();
+	$pubNubConf->setSubscribeKey($pubNub->subscribeKey);
+	$pubNubConf->setPublishKey($pubNub->publishKey);
+	$pubNubConf->setSecretKey($pubNub->secretKey);
+	$pubNubConf->setSecure(true);
+	$pubNubBoard = new PubNub($pubNubConf);
+
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTrace();
+}
+
+// encode and return reply to front end caller
+header("Content-type: application/json");
+echo json_encode($reply);
