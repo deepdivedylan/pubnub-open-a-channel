79a897d9d66a12c80d93c71954867fac3bfe592d
Dylan McDonald <dmcdonald21@cnm.edu>
2018-02-28 10:47:17 -0700

    registered HTTP interceptor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {RouterModule, Routes} from "@angular/router";
 import {SplashComponent} from "./splash/splash.component";
 import {ConstituentComponent} from "./constituent/constituent.component";
 import {SenatorComponent} from "./senator/senator.component";
 import {ConstituentService} from "./shared/services/constituent.service";
 import {SenatorService} from "./shared/services/senator.service";
+import {DeepDiveInterceptor} from "./shared/interceptors/deep.dive.interceptor";
 
 
 export const allAppComponents = [ConstituentComponent, SenatorComponent, SplashComponent];
@@ -15,6 +17,7 @@ export const routes: Routes = [
 ];
 
 export const appRoutingProviders: any[] = [
+	{provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true},
 	ConstituentService,
 	SenatorService
 ];
