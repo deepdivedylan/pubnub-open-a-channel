90ac77197b26e86219fb81ccd45b7e2c3347685f
Dylan McDonald <dmcdonald21@cnm.edu>
2018-02-28 11:50:50 -0700

    injected pubnub into Senator component

--- src/app/senator/senator.component.ts
+++ src/app/senator/senator.component.ts
@@ -1,7 +1,16 @@
-import {Component, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
+import {PubNubAngular} from "pubnub-angular2";
 
 @Component({
 	template: require("./senator.component.html")
 })
 
-export class SenatorComponent {}
+export class SenatorComponent {
+
+	constructor(protected pubnub: PubNubAngular) {
+		this.pubnub.init({
+			publishKey: "pub-c-d8eb3d22-aab2-4526-a633-a6da83bb3ef7",
+			subscribeKey: "sub-c-73e431b8-1cb2-11e8-b6fb-56b8b46ff3aa"
+		});
+	}
+}
