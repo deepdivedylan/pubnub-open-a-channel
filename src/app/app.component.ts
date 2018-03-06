618feef67efeacdd1131dbad698ccc643d364e85
Dylan McDonald <dmcdonald21@cnm.edu>
2018-03-06 15:21:10 -0700

    added status call to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import {Component} from "@angular/core";
+import {SessionService} from "./shared/services/session.service";
+import {Status} from "./shared/classes/status";
 
 @Component({
 	selector: "pubnub-open-a-channel",
 	template: require("./app.component.html")
 })
 
-export class AppComponent {}
+export class AppComponent {
+
+	status: Status = null;
+
+	constructor(protected sessionService: SessionService) {
+		this.sessionService.setSession()
+			.subscribe(status => this.status = status);
+	}
+}
