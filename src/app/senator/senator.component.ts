c66bf1c20f09135018af4de963eb4ab7771b93f5
Dylan McDonald <dmcdonald21@cnm.edu>
2018-03-06 21:49:08 -0700

    deleted WET code from senator component

--- src/app/senator/senator.component.ts
+++ src/app/senator/senator.component.ts
@@ -1,9 +1,7 @@
 import {Component} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {PubNubAngular} from "pubnub-angular2";
-import {SenatorService} from "../shared/services/senator.service";
-import {Message} from "../shared/classes/message";
 import {Status} from "../shared/classes/status";
+import {PubnubService} from "../shared/services/pubnub.service";
 
 @Component({
 	template: require("./senator.component.html")
@@ -11,46 +9,13 @@ import {Status} from "../shared/classes/status";
 
 export class SenatorComponent {
 
-	chatForm: FormGroup;
-	chatMessages: Message[] = [];
 	roomName: string = null;
 	roomForm: FormGroup;
 	status: Status = null;
 
-	constructor(protected formBuilder: FormBuilder, protected pubnub: PubNubAngular, protected senatorService: SenatorService) {
-		this.chatForm = this.formBuilder.group({
-			chatMessage: ["", [Validators.maxLength(1024), Validators.required]]
-		});
+	constructor(protected formBuilder: FormBuilder, protected pubnubService: PubnubService) {
 		this.roomForm = this.formBuilder.group({
 			roomName: ["", [Validators.maxLength(64), Validators.pattern(/^[\da-z-]+$/), Validators.required]]
-		});
-		this.pubnub.init({
-			publishKey: "pub-c-d8eb3d22-aab2-4526-a633-a6da83bb3ef7",
-			subscribeKey: "sub-c-73e431b8-1cb2-11e8-b6fb-56b8b46ff3aa"
-		});
-
-		let parent = this;
-		this.pubnub.addListener({
-			message: function(pubnubMessage : any) {
-				let message = pubnubMessage.message;
-				parent.chatMessages.push(message);
-			}
-		});
-	}
-
-	sendChat(): void {
-		let message = new Message(this.chatForm.value.chatMessage, new Date(), "senator-arlo");
-		this.pubnub.publish({message: message, channel: this.roomName});
-	}
-
-	createRoom(): void {
-		let roomName = this.roomForm.value.roomName;
-		this.senatorService.createRoom(roomName).subscribe(status => {
-			if(status.status === 200) {
-				this.roomName = roomName;
-				this.pubnub.subscribe({channels: [roomName]});
-			}
-			this.status = status;
 		});
 	}
 }
